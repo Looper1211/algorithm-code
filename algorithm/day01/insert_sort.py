15f8103a5a6912808b68ec3b5325dae834b972d1
looper1211 <bitengfei@itcast.cn>
2019-05-11 17:07:35 +0800

    insert_sort update

--- algorithm/day01/insert_sort.py
+++ algorithm/day01/insert_sort.py
@@ -5,15 +5,13 @@ def insert_sort(list1):
     """
     插入排序
     """
-    if list1 is None or len(list1) < 2:
-        return
-    for i in range(0, len(list1)):
+    for i in range(1, len(list1)):
         temp = list1[i]
-        j = i
-        while j > 0 and list1[j - 1] > temp:
-            list1[j] = list1[j - 1]
+        j = i - 1
+        while j >= 0 and temp < list1[j]:
+            list1[j + 1] = list1[j]
             j -= 1
-        list1[j] = temp
+        list1[j + 1] = temp
 
 
 def insert_sort2(list1):
